5fe4cbde0373ed7bb47b73a80fea3aca77202c1b
Arunsh93 <Arunhosur93@gmail.com>
2021-10-13 12:06:10 +0530

    Testing Reflection Cases NoSuchClass and NoSuchMethod

--- MSTestForMoodAnalyser/MoodAnalyserTestClass.cs
+++ MSTestForMoodAnalyser/MoodAnalyserTestClass.cs
@@ -53,5 +53,57 @@ namespace MSTestForMoodAnalyser
                 Assert.AreEqual("Message Should not be Empty", ex.Message);
             }
         }
+
+        //TC-4.1
+        [TestMethod]
+        public void GivenMoodAnalyserObject()
+        {
+            string className = "MoodAnalyserProgram.MoodAnalyser";
+            string constructorName = "MoodAnalyser";
+            object Expected = new MoodAnalyser();
+            object Actual = MoodAnalyserFactory.CreateMoodAnalyserDefaultConstructor(className, constructorName);
+            Expected.Equals(Actual);
+        }
+        //TC-4.2
+        [TestMethod]
+        public void GivenMoodAnalyser_ReturnNoSuchClass()
+        {
+            try
+            {
+                //Arrange
+                string className = "MoodAnalyserProgram.MoodAnalyse1";
+                string constructorName = "MoodAnalyser1";
+                object Expected = new MoodAnalyser();
+
+                //Action
+                object Actual = MoodAnalyserFactory.CreateMoodAnalyserDefaultConstructor(className, constructorName);
+            }
+            catch(MoodAnalysisCustomException ex)
+            {
+                //Assert
+                Assert.AreEqual("Class Not Found", ex.Message);
+            }          
+        }
+
+        //TC-4.3
+        [TestMethod]
+        public void GivenMoodAnalyser_ReturnNoSuchConstructor()
+        {
+            try
+            {
+                //Arrange
+                string className = "MoodAnalyserProgram.MoodAnalyser";
+                string constructorName = "MoodAnalyser";
+                object Expected = new MoodAnalyser();
+
+                //Action
+                object Actual = MoodAnalyserFactory.CreateMoodAnalyserDefaultConstructor(className, constructorName);
+            }
+            catch (MoodAnalysisCustomException ex)
+            {
+                //Assert
+                Assert.AreEqual("Constructor Not Found", ex.Message);
+            }
+        }
     }
 }
